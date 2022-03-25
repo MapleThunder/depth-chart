d020777c48a9b643cc594165faf5582f9635835c
Niko Bentley <niko.bentley@redspace.com>
2022-03-25 16:46:28 -0300

    started stubbing out default state

---
+++ src/hooks/usePlayers.tsx
@@ -0,0 +1,17 @@
+export const default_state = {
+  players: [
+    {
+      name: "Player 1",
+      positions: [
+        {
+          position: "ST",
+          weight: 7
+        },
+        {
+          position: "RW",
+          weight: 4
+        },
+      ],
+    }
+  ]
+};
